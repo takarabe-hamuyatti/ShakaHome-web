44f502a63741e3b12eb32cf45432ee71a08d217c
shota.takarabe <syota.takarabe@gmail.com>
2023-04-14 00:20:39 +0900

    Homeのコンポーネントを経由する形に変更

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+import {PastVideoList} from "@/components/PastVideoList";
+
+export const Home: FC = () => {
+    return (
+        <div>
+            <PastVideoList/>
+        </div>
+    )
+}
