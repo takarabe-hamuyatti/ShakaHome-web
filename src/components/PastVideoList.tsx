328df9584fc1db978bbfacbd91bb69eec947456e
shota.takarabe <syota.takarabe@gmail.com>
2023-04-14 00:17:16 +0900

    emotion cssの導入

--- src/components/PastVideoList.tsx
+++ src/components/PastVideoList.tsx
@@ -1,23 +1,26 @@
 import {FC} from "react";
 import {VideoCard} from "@/components/VideoCard";
+import styled from "@emotion/styled";
+
+const VideoListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 export const PastVideoList: FC = () => {
     return (
-        <div>
-            {
-                Array(9)
-                    .fill(0)
-                    .map(() => dummyPastVideo)
-                    .map((info, index) => (
-                            <VideoCard key={index} imageUrl={info.imageUrl} title={info.title}/>
-                        )
-                    )
-            }
-        </div>
-    )
-}
+        <VideoListContainer>
+            {Array(9)
+                .fill(0)
+                .map(() => dummyPastVideo)
+                .map((info, index) => (
+                    <VideoCard key={index} imageUrl={info.imageUrl} title={info.title} />
+                ))}
+        </VideoListContainer>
+    );
+};
 
 const dummyPastVideo = {
     imageUrl: "/shakahands.jpeg",
     title: "しゃかhandsだよ"
-}
+};
